8db72fd977b59f3e1b3ed260967a8e26865980fc
Roman Zykov <87817259+RomaZykov@users.noreply.github.com>
2023-05-11 16:28:16 +0300

    Add some dependencies

--- app/src/main/java/com/example/moguchi/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/moguchi/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.moguchi
+package com.example.moguchi.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.moguchi.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
