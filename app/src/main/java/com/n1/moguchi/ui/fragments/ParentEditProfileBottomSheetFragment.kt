b48ddf891127739d2ee22223a85d3ba6b8d9795c
Roman Zykov <zyckoff.rom@yandex.ru>
2023-08-24 08:27:42 +0300

    Commit from desktop to laptop

--- app/src/main/java/com/n1/moguchi/ui/fragments/ParentEditProfileBottomSheetFragment.kt
+++ app/src/main/java/com/n1/moguchi/ui/fragments/ParentEditProfileBottomSheetFragment.kt
@@ -1,24 +1,14 @@
 package com.n1.moguchi.ui.fragments
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import com.n1.moguchi.MoguchiBaseApplication
 import com.n1.moguchi.R
-import com.n1.moguchi.data.models.Goal
-import com.n1.moguchi.databinding.FragmentPrimaryBottomSheetBinding
 import com.n1.moguchi.databinding.FragmentProfileEditBottomSheetBinding
-import com.n1.moguchi.ui.ViewModelFactory
-import com.n1.moguchi.ui.viewmodels.PrimaryBottomSheetViewModel
-import javax.inject.Inject
 
 class ParentEditProfileBottomSheetFragment : BottomSheetDialogFragment() {
 
