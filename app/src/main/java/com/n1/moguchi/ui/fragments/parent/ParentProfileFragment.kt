78452a65a6ac7d6301ae5ed17f2952602cd3e867
Roman Zykov <zyckoff.rom@yandex.ru>
2023-09-23 21:23:09 +0300

    Add notifications recyclerView

--- app/src/main/java/com/n1/moguchi/ui/fragments/parent/ParentProfileFragment.kt
+++ app/src/main/java/com/n1/moguchi/ui/fragments/parent/ParentProfileFragment.kt
@@ -8,16 +8,28 @@ import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.n1.moguchi.R
 import com.n1.moguchi.databinding.FragmentParentProfileBinding
 import com.n1.moguchi.ui.ViewModelFactory
+import com.n1.moguchi.ui.adapters.NotificationsRecyclerAdapter
 import com.n1.moguchi.ui.viewmodels.HomeViewModel
 import javax.inject.Inject
 
 class ParentProfileFragment : Fragment() {
 
     private lateinit var binding: FragmentParentProfileBinding
+    private lateinit var notificationsRecyclerAdapter: NotificationsRecyclerAdapter
+
+    private val notificationList = mutableListOf(
+        "Максимка достиг цели «Велосипед»",
+        "Алёна выполнила задачу «Сходить в магазин в пятницу»",
+        "Истёк срок повторения задачи «Написать контрольную минимум на 4» для Алёны",
+        "Алёна достигла цели «Поход в кино с друзьями»",
+        "Максимка достиг цели «Велосипед»"
+    )
 
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
@@ -58,6 +70,34 @@ class ParentProfileFragment : Fragment() {
         binding.myChildrenButton.setOnClickListener {
             navController.navigate(R.id.action_parentProfileFragment_to_addChildFragment)
         }
+
+        binding.deleteNotificationsButton.setOnClickListener {
+            notificationList.clear()
+            notificationsRecyclerAdapter.notifyItemRangeChanged(0, notificationList.size - 1)
+        }
+
+        binding.notificationsSwitch.isChecked = false
+        binding.notificationsSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+            setupRecyclerView(view, notificationList)
+            if (isChecked) {
+                if (notificationsRecyclerAdapter.itemCount != 0) {
+                    binding.deleteNotificationsButton.visibility = View.VISIBLE
+                    binding.emptyNotificationsHint.visibility = View.GONE
+                }
+                binding.rvNotificationsList.visibility = View.VISIBLE
+            } else {
+                binding.emptyNotificationsHint.visibility = View.VISIBLE
+                binding.rvNotificationsList.visibility = View.GONE
+                binding.deleteNotificationsButton.visibility = View.GONE
+            }
+        }
+    }
+
+    private fun setupRecyclerView(view: View, notificationList: MutableList<String>) {
+        val recyclerViewNotifications: RecyclerView = view.findViewById(R.id.rv_notifications_list)
+        recyclerViewNotifications.layoutManager = LinearLayoutManager(requireContext())
+        notificationsRecyclerAdapter = NotificationsRecyclerAdapter(notificationList)
+        recyclerViewNotifications.adapter = notificationsRecyclerAdapter
     }
 
     private fun showBottomSheet(fragment: Fragment) {
