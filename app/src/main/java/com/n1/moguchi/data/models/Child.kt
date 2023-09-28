7ce11f92cf4367b7f223ee1283af4c23429df9f2
Roman Zykov <87817259+RomaZykov@users.noreply.github.com>
2023-09-28 20:47:32 +0300

    Update move from parent to child profile (Child home page)

--- app/src/main/java/com/n1/moguchi/data/models/Child.kt
+++ app/src/main/java/com/n1/moguchi/data/models/Child.kt
@@ -4,5 +4,6 @@ package com.n1.moguchi.data.models
 data class Child(
     val childId: String? = null,
     val parentOwnerId: String? = "",
-    val childName: String = ""
+    val childName: String = "",
+    val isChildMode: Boolean = false
 )
--- app/src/main/java/com/n1/moguchi/ui/fragments/child/HomeChildFragment.kt
+++ app/src/main/java/com/n1/moguchi/ui/fragments/child/HomeChildFragment.kt
@@ -5,29 +5,46 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.n1.moguchi.R
-import com.n1.moguchi.databinding.FragmentHomeBinding
+import com.n1.moguchi.databinding.FragmentChildHomeBinding
+import com.n1.moguchi.ui.activity.MainActivity
 
 class HomeChildFragment : Fragment() {
 
-    private lateinit var binding: FragmentHomeBinding
+    private lateinit var binding: FragmentChildHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        binding = FragmentChildHomeBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val navHostFragment =
-            requireActivity().supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
 
+        MainActivity.isParentProfile = false
 
+        updateBottomNavigationView()
+
+        (activity as MainActivity).findViewById<BottomNavigationView>(R.id.bottom_navigation_view).menu.findItem(R.id.switchToParent).setOnMenuItemClickListener {
+
+            true
+        }
+    }
+
+//    private fun showToParentChangeBottomSheet() {
+//        val fragmentManager = supportFragmentManager
+//        val modalBottomSheet = PrimaryBottomSheetFragment()
+//        val mainActivityTag = MainActivity.TAG
+//        fragmentManager.setFragmentResult("requestKey", bundleOf("bundleKey" to mainActivityTag))
+//        modalBottomSheet.show(fragmentManager, MainActivity.TAG)
+//    }
+
+    private fun updateBottomNavigationView() {
+        (activity as MainActivity).setupBottomNavigationView()
     }
 }
