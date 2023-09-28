3351b4f7b6743ba9dd5ba9764e48768f8d860845
Roman Zykov <87817259+RomaZykov@users.noreply.github.com>
2023-09-28 20:54:10 +0300

    Rework main bottom navigation

--- app/src/main/java/com/n1/moguchi/ui/activity/MainActivity.kt
+++ app/src/main/java/com/n1/moguchi/ui/activity/MainActivity.kt
@@ -1,14 +1,12 @@
-package com.n1.moguchi.ui.activities
+package com.n1.moguchi.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
-import androidx.navigation.findNavController
+import androidx.core.view.isNotEmpty
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupWithNavController
 import com.n1.moguchi.R
 import com.n1.moguchi.databinding.ActivityMainBinding
@@ -16,6 +14,7 @@ import com.n1.moguchi.ui.fragments.parent.PrimaryBottomSheetFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,35 +23,43 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        val appBarConfiguration = AppBarConfiguration(
-            topLevelDestinationIds = setOf(R.id.homeFragment, R.id.chooseChildrenFragment),
-            fallbackOnNavigateUpListener = ::onSupportNavigateUp
-        )
-//        binding.topAppBar.setupWithNavController(navController, appBarConfiguration)
-        binding.bottomNavigationView.setupWithNavController(navController)
-        binding.bottomNavigationView.menu.findItem(R.id.addGoal).setOnMenuItemClickListener {
-            showGoalAndTaskCreationBottomSheet()
-            true
-        }
+            supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        navController = navHostFragment.navController
 
+        setupBottomNavigationView()
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
-                R.id.homeFragment -> {
+                R.id.parentHomeFragment -> {
                     showUi()
                 }
-                R.id.chooseChildrenFragment -> {
+
+                R.id.switchToChildFragment -> {
                     showUi()
                 }
+
+                R.id.childHomeFragment -> {
+                    showUi()
+                }
+
                 else -> hideUi()
             }
         }
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
-        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+    fun setupBottomNavigationView() {
+        binding.bottomNavigationView.setupWithNavController(navController)
+        if (binding.bottomNavigationView.menu.isNotEmpty()) {
+            binding.bottomNavigationView.menu.clear()
+        }
+        if (isParentProfile) {
+            binding.bottomNavigationView.inflateMenu(R.menu.bottom_menu_parent)
+            binding.bottomNavigationView.menu.findItem(R.id.addGoal).setOnMenuItemClickListener {
+                showGoalAndTaskCreationBottomSheet()
+                true
+            }
+        } else {
+            binding.bottomNavigationView.inflateMenu(R.menu.bottom_menu_child)
+        }
     }
 
     private fun showGoalAndTaskCreationBottomSheet() {
@@ -73,5 +80,8 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         private const val TAG = "MainActivity"
+
+        var isParentProfile = true
     }
 }
+
