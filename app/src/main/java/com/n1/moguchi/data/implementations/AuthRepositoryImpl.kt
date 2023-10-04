a272e9bb9b8ab4ec1a684a210b36dd20f0c45101
Roman Zykov <87817259+RomaZykov@users.noreply.github.com>
2023-10-04 16:39:10 +0300

    Delete auth viewModels and repository

--- app/src/main/java/com/n1/moguchi/data/implementations/AuthRepositoryImpl.kt
+++
@@ -1,8 +0,0 @@
-package com.n1.moguchi.data.implementations
-
-import com.n1.moguchi.data.repositories.AuthRepository
-import javax.inject.Inject
-
-class AuthRepositoryImpl @Inject constructor() : AuthRepository {
-
-}
