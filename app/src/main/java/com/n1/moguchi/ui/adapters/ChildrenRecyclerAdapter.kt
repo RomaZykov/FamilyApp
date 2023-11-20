90dfd1785b1352ed5349823d0f5bf129e63c7179
Roman Zykov <zyckoff.rom@yandex.ru>
2023-11-20 22:53:41 +0300

    fix: replace radioGroup list to recyclerView for better implementation

--- app/src/main/java/com/n1/moguchi/ui/adapters/ChildrenRecyclerAdapter.kt
+++ app/src/main/java/com/n1/moguchi/ui/adapters/ChildrenRecyclerAdapter.kt
@@ -1,175 +1,49 @@
 package com.n1.moguchi.ui.adapters
 
-import android.content.Context
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.n1.moguchi.R
 import com.n1.moguchi.data.models.Child
-import com.n1.moguchi.databinding.ChildCreationCardBinding
-import com.n1.moguchi.databinding.CreationChildSectionFooterBinding
 import com.n1.moguchi.databinding.MediumChildItemBinding
+import com.n1.moguchi.databinding.SmallChildItemBinding
 
-class ChildrenRecyclerAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class ChildrenRecyclerAdapter(private val childrenList: List<Child>) : RecyclerView.Adapter<ChildrenRecyclerAdapter.SmallChildViewHolder>() {
 
-    var children: MutableList<Child> = ArrayList()
-        set(value) {
-            field = value
-            if (field.size == 1) {
-                notifyItemChanged(0)
-            }
-        }
-    var onNewChildAddClicked: (() -> Unit)? = null
-    var onChildUpdate: ((Child) -> Unit)? = null
-    var onChildRemoveClicked: ((Child) -> Unit)? = null
-    var onCardsStatusUpdate: ((Boolean) -> Unit)? = null
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return when (viewType) {
-            VIEW_TYPE_CHILD_CARD -> {
-                val view = LayoutInflater.from(parent.context).inflate(
-                    R.layout.child_creation_card,
-                    parent,
-                    false
-                )
-                ChildViewHolder(view)
-            }
-
-            VIEW_TYPE_FOOTER -> {
-                val view = LayoutInflater.from(parent.context).inflate(
-                    R.layout.creation_child_section_footer,
-                    parent,
-                    false
-                )
-                BottomViewHolder(view)
-            }
-
-            else -> {
-                val view = LayoutInflater.from(parent.context).inflate(
-                    R.layout.medium_child_item,
-                    parent,
-                    false
-                )
-                MediumChildViewHolder(view)
-            }
-        }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SmallChildViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.small_child_item, parent, false)
+        return SmallChildViewHolder(view)
     }
 
-    override fun getItemViewType(position: Int): Int {
-        return if (position == children.size) {
-            VIEW_TYPE_FOOTER
-        } else {
-            VIEW_TYPE_CHILD_CARD
-        }
-    }
+    override fun onBindViewHolder(holder: SmallChildViewHolder, position: Int) {
+        val child: Child = childrenList[position]
+        holder.bind(child)
 
-    override fun onBindViewHolder(
-        holder: RecyclerView.ViewHolder,
-        position: Int
-    ) {
-        when (holder.itemViewType) {
-            VIEW_TYPE_CHILD_CARD -> (holder as ChildViewHolder).bind()
-            VIEW_TYPE_FOOTER -> (holder as BottomViewHolder).bind()
-            else -> throw RuntimeException("Unknown viewType: ${holder.itemViewType}")
-        }
-//        else {
-//            (holder as MediumChildViewHolder).bind(child as String)
+//        else -> {
+//            val view = LayoutInflater.from(parent.context).inflate(
+//                R.layout.medium_child_item,
+//                parent,
+//                false
+//            )
+//            MediumChildViewHolder(view)
 //        }
     }
 
     override fun getItemCount(): Int {
-        return children.size + 1
+        return childrenList.size
     }
 
-    inner class ChildViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val binding = ChildCreationCardBinding.bind(itemView)
-        private val childAvatars: Map<Int, Int> = mapOf(
-           binding.avatarMale1.id to R.drawable.avatar_male_1,
-           binding.avatarMale2.id to R.drawable.avatar_male_2,
-           binding.avatarFemale2.id to R.drawable.avatar_female_2,
-           binding.avatarFemale3.id to R.drawable.avatar_female_3
-        )
+    inner class SmallChildViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val binding = SmallChildItemBinding.bind(itemView)
 
-        fun bind() {
-            binding.childNameEditText.addTextChangedListener(object : TextWatcher {
-                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                }
-
-                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                }
-
-                override fun afterTextChanged(childName: Editable?) {
-                    children[adapterPosition].childName = childName.toString()
-                    if (childName.toString().isEmpty()) {
-                        binding.childNameEditText.error = "Выполните все условия"
-                    } else {
-                        onChildUpdate?.invoke(children[adapterPosition])
-                    }
-                    notifyItemChanged(itemCount - 1)
-                }
-            })
-
-            if (children.size > 1) {
-                binding.deleteChildButton.visibility = View.VISIBLE
-                binding.deleteChildButton.setOnClickListener {
-                    onChildRemoveClicked?.invoke(children[adapterPosition])
-                    children.removeAt(adapterPosition)
-                    notifyItemRemoved(adapterPosition)
-                    notifyItemChanged(itemCount - 1)
-                }
-            } else {
-                binding.deleteChildButton.visibility = View.GONE
-            }
-
-            binding.avatars.setOnCheckedChangeListener { _, checkedId ->
-                when (checkedId) {
-                    binding.avatarMale1.id,
-                    binding.avatarMale2.id,
-                    binding.avatarFemale2.id,
-                    binding.avatarFemale3.id -> {
-                        children[adapterPosition].imageResourceId = childAvatars[checkedId]
-                        onChildUpdate?.invoke(children[adapterPosition])
-                        notifyItemChanged(itemCount - 1)
-                    }
-                }
-            }
+        fun bind(child: Child) {
+            binding.smallChildName.text = child.childName
+            binding.smallChildAvatar.setImageResource(child.imageResourceId!!)
         }
     }
 
-    inner class BottomViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val binding = CreationChildSectionFooterBinding.bind(itemView)
-        var context: Context = itemView.context
-
-        fun bind() {
-            if (children.all {
-                    it.childName.toString().isNotEmpty() && it.imageResourceId != null
-                } && children.size == itemCount - 1) {
-                onCardsStatusUpdate?.invoke(true)
-                with(binding.addChildButton) {
-                    isEnabled = true
-                    setTextColor(context.getColorStateList(R.color.black))
-                    iconTint = context.getColorStateList(R.color.black)
-                    backgroundTintList = context.getColorStateList(R.color.white)
-                }
-                itemView.setOnClickListener {
-                    onNewChildAddClicked?.invoke()
-                }
-            } else {
-                onCardsStatusUpdate?.invoke(false)
-                with(binding.addChildButton) {
-                    isEnabled = false
-                    backgroundTintList = context.getColorStateList(R.color.white_opacity_70)
-                    setTextColor(context.getColorStateList(R.color.black_opacity_70))
-                    iconTint = context.getColorStateList(R.color.black_opacity_70)
-                }
-            }
-        }
-    }
-
-    // Need to remove
     inner class MediumChildViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val binding = MediumChildItemBinding.bind(itemView)
 
@@ -184,11 +58,5 @@ class ChildrenRecyclerAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() 
 //                childClickListener?.onChildItemClick(itemView)
 //            }
         }
-    }
-
-    companion object {
-        const val MAX_POOL_SIZE = 0
-        const val VIEW_TYPE_CHILD_CARD = 100
-        const val VIEW_TYPE_FOOTER = 101
     }
 }
--- app/src/main/java/com/n1/moguchi/ui/fragments/parent/GoalCreationFragment.kt
+++ app/src/main/java/com/n1/moguchi/ui/fragments/parent/GoalCreationFragment.kt
@@ -7,19 +7,18 @@ import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.RadioGroup
-import android.widget.TextView
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.google.android.material.card.MaterialCardView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.n1.moguchi.MoguchiBaseApplication
-import com.n1.moguchi.R
+import com.n1.moguchi.data.models.Child
 import com.n1.moguchi.databinding.FragmentGoalCreationBinding
 import com.n1.moguchi.ui.ViewModelFactory
+import com.n1.moguchi.ui.adapters.ChildrenRecyclerAdapter
 import com.n1.moguchi.ui.viewmodels.GoalCreationViewModel
 import javax.inject.Inject
 
@@ -27,6 +26,7 @@ class GoalCreationFragment : Fragment() {
 
     private var _binding: FragmentGoalCreationBinding? = null
     private val binding get() = _binding!!
+    private lateinit var childrenAdapter: ChildrenRecyclerAdapter
 
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
@@ -58,30 +58,13 @@ class GoalCreationFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val parentId = Firebase.auth.currentUser?.uid
 
-        val childrenRadioGroup = view.findViewById<RadioGroup>(R.id.children_radio_group)
         if (parentId != null) {
             viewModel.getChildren(parentId)
             viewModel.children.observe(viewLifecycleOwner) { children ->
-                children?.forEach { child ->
-                    val childItem =
-                        layoutInflater.inflate(
-                            R.layout.small_child_item,
-                            childrenRadioGroup,
-                            false
-                        )
-                    childItem.findViewById<TextView>(R.id.small_child_name).text = child.childName
-                    childItem.findViewById<ImageView>(R.id.small_child_avatar)
-                        .setImageResource(child.imageResourceId!!)
-                    childrenRadioGroup.addView(childItem)
-                    (childrenRadioGroup.getChildAt(0) as MaterialCardView).isChecked = true
-                }
+                setupRecyclerView(children)
             }
         } else {
             throw Exception("User not authorized")
-        }
-
-        childrenRadioGroup.setOnCheckedChangeListener { radioGroup, checkedId ->
-            (radioGroup.getChildAt(1) as MaterialCardView).isChecked = true
         }
 
         binding.goalName.addTextChangedListener(object : TextWatcher {
@@ -118,18 +101,14 @@ class GoalCreationFragment : Fragment() {
                 viewModel.decreaseHeight()
             }
         }
+    }
 
-//        binding.nextButton.setOnClickListener {
-//            val currentGoalHeight = viewModel.goalHeight.value
-//            val goal = Goal(
-//
-//            )
-////            viewModel.createGoal(goal = goal)
-//            parentFragmentManager.beginTransaction()
-//                .remove(GoalCreationFragment())
-//                .replace(R.id.child_fragment_container, TaskCreationFragment())
-//                .commit()
-//        }
+    private fun setupRecyclerView(children: List<Child>) {
+        val recyclerView: RecyclerView = binding.rvChildrenList
+        recyclerView.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        childrenAdapter = ChildrenRecyclerAdapter(children)
+        recyclerView.adapter = childrenAdapter
     }
 
     override fun onDestroyView() {
