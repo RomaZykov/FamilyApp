b218d3ceb05cf52f805aff2bc0b51ac37cadf741
Roman Zykov <zyckoff.rom@yandex.ru>
2023-11-27 21:16:27 +0300

    feat: implement password setting (WIP)

--- app/src/main/java/com/n1/moguchi/ui/fragments/PasswordFragment.kt
+++ app/src/main/java/com/n1/moguchi/ui/fragments/PasswordFragment.kt
@@ -1,9 +1,12 @@
 package com.n1.moguchi.ui.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import com.n1.moguchi.databinding.FragmentPasswordBinding
 
@@ -24,6 +27,30 @@ class PasswordFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.passwordForChildEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(password: Editable?) {
+                if (password.toString().isEmpty()) {
+                    binding.passwordForChildEditText.error = "Нужно установить код"
+                    parentFragmentManager.clearFragmentResult("buttonIsEnabled")
+                    parentFragmentManager.setFragmentResult(
+                        "buttonIsEnabled",
+                        bundleOf("buttonIsReadyKey" to false)
+                    )
+                } else {
+                    parentFragmentManager.clearFragmentResult("buttonIsEnabled")
+                    parentFragmentManager.setFragmentResult(
+                        "buttonIsEnabled",
+                        bundleOf("buttonIsReadyKey" to true)
+                    )
+                }
+            }
+        })
     }
 
     override fun onDestroyView() {
