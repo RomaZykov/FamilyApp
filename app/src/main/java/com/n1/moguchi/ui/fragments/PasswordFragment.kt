36ba4fd7c139db8dfe62d72cd70114af0a5cbe3d
Roman Zykov <87817259+RomaZykov@users.noreply.github.com>
2023-12-02 17:46:17 +0300

    Update password fragment

--- app/src/main/java/com/n1/moguchi/ui/fragments/PasswordFragment.kt
+++ app/src/main/java/com/n1/moguchi/ui/fragments/PasswordFragment.kt
@@ -1,19 +1,43 @@
 package com.n1.moguchi.ui.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.n1.moguchi.MoguchiBaseApplication
 import com.n1.moguchi.databinding.FragmentPasswordBinding
+import com.n1.moguchi.ui.ViewModelFactory
+import com.n1.moguchi.ui.fragments.parent.goal_creation.GoalCreationFragment
+import com.n1.moguchi.ui.viewmodels.PasswordViewModel
+import javax.inject.Inject
 
 class PasswordFragment : Fragment() {
 
     private var _binding: FragmentPasswordBinding? = null
     private val binding get() = _binding!!
+    private var isNextButtonPressed: Boolean? = null
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+    private val viewModel by lazy {
+        ViewModelProvider(this, viewModelFactory)[PasswordViewModel::class.java]
+    }
+
+    private val component by lazy {
+        (requireActivity().application as MoguchiBaseApplication).appComponent
+    }
+
+    override fun onAttach(context: Context) {
+        component.inject(this)
+        super.onAttach(context)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,6 +45,7 @@ class PasswordFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentPasswordBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
         return binding.root
     }
 
@@ -35,7 +60,7 @@ class PasswordFragment : Fragment() {
             }
 
             override fun afterTextChanged(password: Editable?) {
-                if (password.toString().isEmpty()) {
+                if (password.toString().isEmpty() && password.toString().isBlank()) {
                     binding.passwordForChildEditText.error = "Нужно установить код"
                     parentFragmentManager.clearFragmentResult("buttonIsEnabled")
                     parentFragmentManager.setFragmentResult(
@@ -51,6 +76,20 @@ class PasswordFragment : Fragment() {
                 }
             }
         })
+
+        parentFragmentManager.setFragmentResultListener(
+            "nextButtonPressed",
+            viewLifecycleOwner
+        ) { _, bundle ->
+            isNextButtonPressed = bundle.getBoolean("buttonIsPressedKey")
+            if (isNextButtonPressed == true) {
+                GoalCreationFragment.selectedChildIndex++
+                val currentChildID = requireParentFragment().arguments?.getString(GoalCreationFragment.CHILD_ID_KEY)
+                val password = binding.passwordForChildEditText.text.toString().toInt()
+                Log.d("CommonFragment", "CurrentChildID = $currentChildID, selectedChildIndex = ${GoalCreationFragment.selectedChildIndex}")
+                viewModel.setPassword(password, currentChildID!!)
+            }
+        }
     }
 
     override fun onDestroyView() {
