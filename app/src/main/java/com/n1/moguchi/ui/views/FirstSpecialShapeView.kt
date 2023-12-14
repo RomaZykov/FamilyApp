4ff36ae57cddb78a7047bbf4d2d379fa810aa16a
Roman Zykov <zyckoff.rom@yandex.ru>
2023-12-14 21:07:51 +0300

    add special shape view (WIP) and update custom progress bar with inheritance

--- app/src/main/java/com/n1/moguchi/ui/views/FirstSpecialShapeView.kt
+++ app/src/main/java/com/n1/moguchi/ui/views/FirstSpecialShapeView.kt
@@ -2,20 +2,28 @@ package com.n1.moguchi.ui.views
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.CornerPathEffect
 import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
-import com.n1.moguchi.R
 
 class FirstSpecialShapeView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
     private val fillPaint = Paint().apply {
-        color = context.getColor(R.color.blue)
+        color = context.getColor(com.n1.moguchi.R.color.blue)
         style = Paint.Style.FILL
         strokeWidth = 8f
+        pathEffect = CornerPathEffect(72f)
     }
     private val path: Path = Path()
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val width = MeasureSpec.getSize(widthMeasureSpec)
+        val height = MeasureSpec.getSize(heightMeasureSpec)
+        setMeasuredDimension(width, height)
+    }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
@@ -24,51 +32,34 @@ class FirstSpecialShapeView(context: Context, attrs: AttributeSet) : View(contex
         val left = paddingLeft
         val right = width - paddingRight
 
-        val centerHorizontal = width / 2f // total = 412
-        val bottomVertical = height.toFloat() // total = 915
-        val sides = 6
-        val angle = 2.0 * Math.PI / 5
-        path.moveTo(centerHorizontal, bottomVertical - 50)
-        // /
-        path.lineTo(
-            centerHorizontal + (width / 2) - 50f,
-            bottomVertical - 90f
-        )
-        // |
-        path.lineTo(
-            centerHorizontal + (width / 2) - 50f,
-            90f
-        )
-        // \
-        path.lineTo(
-            centerHorizontal,
-            45f
-        )
-        // /
-        path.lineTo(
-            centerHorizontal - (width / 2) + 50f,
-            90f
-        )
-        // |
-//        path.lineTo(
-//            centerHorizontal - (width / 2) + 50f,
-//            bottomVertical - 800
-//        )
-        // \
-        path.lineTo(
-            centerHorizontal - (width / 2) + 50f,
-            bottomVertical - 90
-        )
-//        for (i in 1 until sides) {
-//        }
-        path.close()
+        val centerHorizontal = width / 2f
+        val bottomVertical = height.toFloat()
+        path.apply {
+            val margin = 50f
+            reset()
+            moveTo(centerHorizontal, bottomVertical - margin)
+            lineTo(
+                centerHorizontal + (width / 2) - margin,
+                bottomVertical - 90
+            )
+            lineTo(
+                centerHorizontal + (width / 2) - margin,
+                90f
+            )
+            lineTo(
+                centerHorizontal,
+                15f
+            )
+            lineTo(
+                centerHorizontal - (width / 2) + margin,
+                90f
+            )
+            lineTo(
+                centerHorizontal - (width / 2) + margin,
+                bottomVertical - 90
+            )
+            close()
+        }
         canvas.drawPath(path, fillPaint)
     }
-
-    // region Extensions Utils
-    private fun Float.dpToPx(): Float =
-        this * resources.displayMetrics.density
-
-    private fun Float.pxToDp(): Float =
-        this / resources.displayMetrics.density
 }
