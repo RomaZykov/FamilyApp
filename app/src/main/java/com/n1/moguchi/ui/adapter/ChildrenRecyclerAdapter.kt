bf2a14632f487e5b8b60e32a7d17ff7d37bf515d
Roman Zykov <87817259+RomaZykov@users.noreply.github.com>
2023-12-25 17:31:27 +0300

    update switch to child page

--- app/src/main/java/com/n1/moguchi/ui/adapter/ChildrenRecyclerAdapter.kt
+++ app/src/main/java/com/n1/moguchi/ui/adapter/ChildrenRecyclerAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.n1.moguchi.R
 import com.n1.moguchi.data.models.Child
+import com.n1.moguchi.databinding.MediumChildItemBinding
 import com.n1.moguchi.databinding.SmallAddChildButtonBinding
 import com.n1.moguchi.databinding.SmallChildItemBinding
 
@@ -16,6 +17,8 @@ class ChildrenRecyclerAdapter(
     private val selectedChildIndex: Int
 ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
+    constructor(childrenList: List<Child>) : this(childrenList, selectedChildIndex = -1)
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return when (viewType) {
             VIEW_TYPE_SMALL_CHILD_CARD -> {
@@ -25,6 +28,15 @@ class ChildrenRecyclerAdapter(
                     false
                 )
                 SmallChildViewHolder(view)
+            }
+
+            VIEW_TYPE_MEDIUM_CHILD_CARD -> {
+                val view = LayoutInflater.from(parent.context).inflate(
+                    R.layout.medium_child_item,
+                    parent,
+                    false
+                )
+                MediumChildViewHolder(view)
             }
 
             VIEW_TYPE_BUTTON -> {
@@ -49,7 +61,13 @@ class ChildrenRecyclerAdapter(
                 (holder as SmallChildViewHolder).bind(child, position)
             }
 
+            VIEW_TYPE_MEDIUM_CHILD_CARD -> {
+                val child: Child = childrenList[position]
+                (holder as MediumChildViewHolder).bind(child)
+            }
+
             VIEW_TYPE_BUTTON -> (holder as SmallButtonViewHolder).bind()
+
             else -> throw RuntimeException("Unknown viewType: ${holder.itemViewType}")
         }
     }
@@ -58,12 +76,20 @@ class ChildrenRecyclerAdapter(
         return if (position == childrenList.size) {
             VIEW_TYPE_BUTTON
         } else {
-            VIEW_TYPE_SMALL_CHILD_CARD
+            if (selectedChildIndex != -1) {
+                VIEW_TYPE_SMALL_CHILD_CARD
+            } else {
+                VIEW_TYPE_MEDIUM_CHILD_CARD
+            }
         }
     }
 
     override fun getItemCount(): Int {
-        return childrenList.size + ADD_CHILD_BUTTON
+        return if (selectedChildIndex != -1) {
+            childrenList.size + ADD_CHILD_BUTTON
+        } else {
+            childrenList.size
+        }
     }
 
     inner class SmallChildViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -76,6 +102,15 @@ class ChildrenRecyclerAdapter(
         }
     }
 
+    inner class MediumChildViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val binding = MediumChildItemBinding.bind(itemView)
+
+        fun bind(child: Child) {
+            binding.childName.text = child.childName
+            binding.mediumChildAvatar.setImageResource(child.imageResourceId!!)
+        }
+    }
+
     inner class SmallButtonViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val binding = SmallAddChildButtonBinding.bind(itemView)
 
@@ -84,7 +119,8 @@ class ChildrenRecyclerAdapter(
     }
 
     companion object {
-        private const val VIEW_TYPE_SMALL_CHILD_CARD = 100
-        private const val VIEW_TYPE_BUTTON = 101
+        private const val VIEW_TYPE_MEDIUM_CHILD_CARD = 100
+        private const val VIEW_TYPE_SMALL_CHILD_CARD = 101
+        private const val VIEW_TYPE_BUTTON = 102
     }
 }
--- app/src/main/java/com/n1/moguchi/ui/fragment/parent/SwitchToChildBottomSheetFragment.kt
+++ app/src/main/java/com/n1/moguchi/ui/fragment/parent/SwitchToChildBottomSheetFragment.kt
@@ -9,20 +9,18 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.n1.moguchi.R
+import com.n1.moguchi.data.models.Child
 import com.n1.moguchi.databinding.FragmentSwitchToChildBinding
-import com.n1.moguchi.ui.ChildClickListener
-import com.n1.moguchi.ui.activity.MainActivity
-import com.n1.moguchi.ui.adapter.ChildrenCreationRecyclerAdapter
+import com.n1.moguchi.ui.adapter.ChildrenRecyclerAdapter
 
-class SwitchToChildBottomSheetFragment : BottomSheetDialogFragment(), ChildClickListener {
+class SwitchToChildBottomSheetFragment(private val children: List<Child>) : BottomSheetDialogFragment() {
 
     private var _binding: FragmentSwitchToChildBinding? = null
     private val binding get() = _binding!!
-    private lateinit var childrenCreationRecyclerAdapter: ChildrenCreationRecyclerAdapter
-    lateinit var listener: ChildClickListener
-
-    private var mockNames: List<String> = listOf("Максимка", "Алёна")
+    private lateinit var childrenRecyclerAdapter: ChildrenRecyclerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,15 +35,20 @@ class SwitchToChildBottomSheetFragment : BottomSheetDialogFragment(), ChildClick
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val modalBottomSheet = view.findViewById<ConstraintLayout>(R.id.switch_to_child_bottom_sheet)
+        val parentID = Firebase.auth.currentUser?.uid
+
+        val modalBottomSheet =
+            view.findViewById<ConstraintLayout>(R.id.switch_to_child_bottom_sheet)
         val bottomSheetBehavior = BottomSheetBehavior.from(modalBottomSheet)
         bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
-        listener = this
 
-        val recyclerView: RecyclerView = binding.rvChildrenList
-        recyclerView.layoutManager = LinearLayoutManager(requireContext())
-//        childrenCreationRecyclerAdapter = ChildrenCreationRecyclerAdapter(mockNames.toMutableList(), listener)
-        recyclerView.adapter = childrenCreationRecyclerAdapter
+        if (parentID != null) {
+
+            val recyclerView: RecyclerView = binding.rvChildrenList
+            recyclerView.layoutManager = LinearLayoutManager(requireContext())
+            childrenRecyclerAdapter = ChildrenRecyclerAdapter(children)
+            recyclerView.adapter = childrenRecyclerAdapter
+        }
 
         binding.cancelButton.setOnClickListener {
             dismiss()
@@ -55,10 +58,5 @@ class SwitchToChildBottomSheetFragment : BottomSheetDialogFragment(), ChildClick
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    override fun onChildItemClick(view: View) {
-        dismiss()
-        (activity as MainActivity).navController.navigate(R.id.action_parentHomeFragment_to_onBoardingChildFragment)
     }
 }
