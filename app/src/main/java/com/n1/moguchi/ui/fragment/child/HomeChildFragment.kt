f976ebf6ef18b794df90eb96e16b2ca8ea2fc4c6
Roman Zykov <87817259+RomaZykov@users.noreply.github.com>
2023-12-27 16:19:35 +0300

    refactoring layout (DONE)

--- app/src/main/java/com/n1/moguchi/ui/fragment/child/HomeChildFragment.kt
+++ app/src/main/java/com/n1/moguchi/ui/fragment/child/HomeChildFragment.kt
@@ -5,8 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.n1.moguchi.R
 import com.n1.moguchi.databinding.FragmentChildHomeBinding
