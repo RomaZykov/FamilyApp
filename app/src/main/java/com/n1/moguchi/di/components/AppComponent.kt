5e308fe6caf47afbb77f4c2774fad0012fd2779f
Roman Zykov <87817259+RomaZykov@users.noreply.github.com>
2024-01-09 20:05:48 +0300

    remove superfluous injecting
    Signed-off-by: Roman Zykov <87817259+RomaZykov@users.noreply.github.com>

--- app/src/main/java/com/n1/moguchi/di/components/AppComponent.kt
+++ app/src/main/java/com/n1/moguchi/di/components/AppComponent.kt
@@ -13,7 +13,6 @@ import com.n1.moguchi.ui.fragment.parent.password.PasswordFragment
 import com.n1.moguchi.ui.fragment.parent.children_creation.ChildCreationFragment
 import com.n1.moguchi.ui.fragment.parent.goal_creation.GoalCreationFragment
 import com.n1.moguchi.ui.fragment.parent.home.ParentHomeFragment
-import com.n1.moguchi.ui.fragment.parent.PrimaryBottomSheetFragment
 import com.n1.moguchi.ui.fragment.parent.task_creation.TaskCreationFragment
 import com.n1.moguchi.ui.fragment.tasks.TasksFragment
 import dagger.BindsInstance
@@ -32,8 +31,6 @@ interface AppComponent {
     fun inject(passwordFragment: PasswordFragment)
 
     fun inject(goalCreationFragment: GoalCreationFragment)
-
-    fun inject(bottomSheetFragment: PrimaryBottomSheetFragment)
 
     fun inject(taskCreationFragment: TaskCreationFragment)
 
