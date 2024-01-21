fd5ef4fd0be141cb8f0fd7e94a65994f69a2e2cb
Roman Zykov <87817259+RomaZykov@users.noreply.github.com>
2024-01-21 20:45:27 +0300

    remove unnecessary not implemented features: notifications
    Signed-off-by: Roman Zykov <87817259+RomaZykov@users.noreply.github.com>

--- app/src/main/java/com/n1/moguchi/ui/fragment/parent/profile/ParentProfileFragment.kt
+++ app/src/main/java/com/n1/moguchi/ui/fragment/parent/profile/ParentProfileFragment.kt
@@ -1,4 +1,4 @@
-package com.n1.moguchi.ui.fragment.parent
+package com.n1.moguchi.ui.fragment.parent.profile
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,13 +9,10 @@ import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.n1.moguchi.R
 import com.n1.moguchi.databinding.FragmentParentProfileBinding
 import com.n1.moguchi.ui.ViewModelFactory
-import com.n1.moguchi.ui.adapter.NotificationsRecyclerAdapter
 import com.n1.moguchi.ui.fragment.parent.home.ParentHomeViewModel
 import javax.inject.Inject
 
@@ -23,20 +20,11 @@ class ParentProfileFragment : Fragment() {
 
     private var _binding: FragmentParentProfileBinding? = null
     private val binding get() = _binding!!
-    private lateinit var notificationsRecyclerAdapter: NotificationsRecyclerAdapter
-
-    private val notificationList = mutableListOf(
-        "Максимка достиг цели «Велосипед»",
-        "Алёна выполнила задачу «Сходить в магазин в пятницу»",
-        "Истёк срок повторения задачи «Написать контрольную минимум на 4» для Алёны",
-        "Алёна достигла цели «Поход в кино с друзьями»",
-        "Максимка достиг цели «Велосипед»"
-    )
 
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
     private val viewModel by lazy {
-        ViewModelProvider(this, viewModelFactory)[ParentHomeViewModel::class.java]
+//        ViewModelProvider(this, viewModelFactory)[ParentHomeViewModel::class.java] - Incorrect vm
     }
 
     override fun onCreateView(
@@ -73,39 +61,11 @@ class ParentProfileFragment : Fragment() {
             val bundle = bundleOf("isFromParentProfile" to true)
             navController.navigate(R.id.action_parentProfileFragment_to_addChildFragment, bundle)
         }
-
-        binding.deleteNotificationsButton.setOnClickListener {
-            notificationList.clear()
-            notificationsRecyclerAdapter.notifyItemRangeChanged(0, notificationList.size - 1)
-        }
-
-        binding.notificationsSwitch.isChecked = false
-        binding.notificationsSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
-            setupRecyclerView(view, notificationList)
-            if (isChecked) {
-                if (notificationsRecyclerAdapter.itemCount != 0) {
-                    binding.deleteNotificationsButton.visibility = View.VISIBLE
-                    binding.emptyNotificationsHint.visibility = View.VISIBLE
-                }
-                binding.rvNotificationsList.visibility = View.VISIBLE
-            } else {
-                binding.emptyNotificationsHint.visibility = View.VISIBLE
-                binding.rvNotificationsList.visibility = View.GONE
-                binding.deleteNotificationsButton.visibility = View.GONE
-            }
-        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    private fun setupRecyclerView(view: View, notificationList: MutableList<String>) {
-        val recyclerViewNotifications: RecyclerView = view.findViewById(R.id.rv_notifications_list)
-        recyclerViewNotifications.layoutManager = LinearLayoutManager(requireContext())
-        notificationsRecyclerAdapter = NotificationsRecyclerAdapter(notificationList)
-        recyclerViewNotifications.adapter = notificationsRecyclerAdapter
     }
 
     private fun showBottomSheet(fragment: Fragment) {
