cd6e2ab02dbf56b6e2cfdabf47b6176f1e58470e
Roman Zykov <87817259+RomaZykov@users.noreply.github.com>
2024-01-26 18:39:17 +0300

    update di components
    Signed-off-by: Roman Zykov <87817259+RomaZykov@users.noreply.github.com>

--- app/src/main/java/com/n1/moguchi/di/components/AppComponent.kt
+++ app/src/main/java/com/n1/moguchi/di/components/AppComponent.kt
@@ -7,27 +7,41 @@ import com.n1.moguchi.di.modules.ViewModelModule
 import com.n1.moguchi.di.modules.child_user.ChildUserModule
 import com.n1.moguchi.di.modules.goal.GoalModule
 import com.n1.moguchi.di.modules.parent_user.ParentUserModule
+import com.n1.moguchi.di.modules.switch_to_user.SwitchToUserModule
 import com.n1.moguchi.di.modules.task.TaskModule
 import com.n1.moguchi.ui.activity.MainActivity
+import com.n1.moguchi.ui.fragment.child.home.HomeChildFragment
 import com.n1.moguchi.ui.fragment.parent.password.PasswordFragment
 import com.n1.moguchi.ui.fragment.parent.children_creation.ChildCreationFragment
 import com.n1.moguchi.ui.fragment.parent.goal_creation.GoalCreationFragment
-import com.n1.moguchi.ui.fragment.parent.home.ParentHomeFragment
+import com.n1.moguchi.ui.fragment.parent.home.HomeParentFragment
 import com.n1.moguchi.ui.fragment.parent.task_creation.TaskCreationFragment
-import com.n1.moguchi.ui.fragment.switch_to_user.SwitchToUserBottomSheetFragment
+import com.n1.moguchi.ui.fragment.switch_to_user.SwitchToChildBottomSheetFragment
+import com.n1.moguchi.ui.fragment.switch_to_user.SwitchToParentBottomSheetFragment
 import com.n1.moguchi.ui.fragment.tasks.TasksFragment
 import dagger.BindsInstance
 import dagger.Component
 
 @ApplicationScope
-@Component(modules = [GoalModule::class, TaskModule::class, ChildUserModule::class, ParentUserModule::class, FirebaseModule::class, ViewModelModule::class])
+@Component(
+    modules = [
+        GoalModule::class,
+        TaskModule::class,
+        ChildUserModule::class,
+        ParentUserModule::class,
+        FirebaseModule::class,
+        SwitchToUserModule::class,
+        ViewModelModule::class]
+)
 interface AppComponent {
 
     fun inject(activity: MainActivity)
 
     fun inject(childCreationFragment: ChildCreationFragment)
 
-    fun inject(switchToUserBottomSheetFragment: SwitchToUserBottomSheetFragment)
+    fun inject(switchToChildBottomSheetFragment: SwitchToChildBottomSheetFragment)
+
+    fun inject(switchToParentBottomSheetFragment: SwitchToParentBottomSheetFragment)
 
     fun inject(tasksFragment: TasksFragment)
 
@@ -37,7 +51,9 @@ interface AppComponent {
 
     fun inject(taskCreationFragment: TaskCreationFragment)
 
-    fun inject(parentHomeFragment: ParentHomeFragment)
+    fun inject(homeParentFragment: HomeParentFragment)
+
+    fun inject(homeChildFragment: HomeChildFragment)
 
     fun inject(moguchiBaseApplication: MoguchiBaseApplication)
 
--- app/src/main/java/com/n1/moguchi/di/modules/goal/GoalModule.kt
+++ app/src/main/java/com/n1/moguchi/di/modules/goal/GoalModule.kt
@@ -1,15 +1,25 @@
 package com.n1.moguchi.di.modules.goal
 
+import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import com.n1.moguchi.data.impl.GoalRepositoryImpl
 import com.n1.moguchi.data.repositories.GoalRepository
 import com.n1.moguchi.di.components.ApplicationScope
+import com.n1.moguchi.di.modules.ViewModelKey
+import com.n1.moguchi.ui.fragment.parent.goal_creation.GoalCreationViewModel
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
+import dagger.multibindings.IntoMap
 
 @Module
 interface GoalModule {
+
+    @IntoMap
+    @ViewModelKey(GoalCreationViewModel::class)
+    @Binds
+    fun bindGoalCreationDialogViewModel(bottomSheetViewModel: GoalCreationViewModel): ViewModel
 
     companion object {
 
