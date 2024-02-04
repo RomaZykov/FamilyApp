9852b6b7d9a9e768a3a305052dd62de002708d41
Roman Zykov <87817259+RomaZykov@users.noreply.github.com>
2024-02-04 16:22:12 +0300

    Implement child avatar visibility on top app bar (WIP)
    Signed-off-by: Roman Zykov <87817259+RomaZykov@users.noreply.github.com>

--- app/src/main/java/com/n1/moguchi/data/repositories/ChildRepository.kt
+++ app/src/main/java/com/n1/moguchi/data/repositories/ChildRepository.kt
@@ -1,5 +1,9 @@
 package com.n1.moguchi.data.repositories
 
+import com.n1.moguchi.data.models.Child
+import kotlinx.coroutines.flow.Flow
+
 interface ChildRepository {
 
+    suspend fun getChild(childId: String): Flow<Child>
 }
