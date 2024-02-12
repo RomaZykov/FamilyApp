7344769ebf23104cdd647887b40d9dea531b40af
Roman Zykov <zyckoff.rom@yandex.ru>
2024-02-13 01:22:14 +0300

    Update firebase api for password reset
    Need to think about correctives

---
+++ app/src/main/java/com/n1/moguchi/api/ApiFirebaseService.kt
@@ -0,0 +1,10 @@
+package com.n1.moguchi.api
+
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface ApiFirebaseService {
+
+//    @POST("user/reset-password")
+//    suspend fun sendPasswordResetEmail(@Body data: Map<String, String>): MoguchiResponse<Void>
+}
