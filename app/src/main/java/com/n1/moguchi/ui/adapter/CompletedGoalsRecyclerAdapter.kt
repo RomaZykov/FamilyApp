47cec675581ada2c484889b7e1b5223ed8209106
Roman Zykov <zyckoff.rom@yandex.ru>
2024-04-13 22:05:36 +0300

    Add button listener for tasks in goal card

--- app/src/main/java/com/n1/moguchi/ui/adapter/CompletedGoalsRecyclerAdapter.kt
+++ app/src/main/java/com/n1/moguchi/ui/adapter/CompletedGoalsRecyclerAdapter.kt
@@ -12,6 +12,8 @@ import com.n1.moguchi.databinding.CompletedGoalCardBinding
 class CompletedGoalsRecyclerAdapter(private val completedGoalsList: List<Goal>) :
     RecyclerView.Adapter<ViewHolder>() {
 
+    var onTasksHistoryClicked: ((String) -> Unit)? = null
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.completed_goal_card, parent, false)
@@ -34,7 +36,14 @@ class CompletedGoalsRecyclerAdapter(private val completedGoalsList: List<Goal>) 
         fun bind(goal: Goal) {
             this.goal = goal
             binding.goalTitle.text = goal.title
-//            binding.allCompletedTasksButton.setOnClickListener(this)
+            binding.goalPointsLayout.goalPoints.text = itemView.context.getString(
+                R.string.current_total_goal_points,
+                goal.currentPoints,
+                goal.totalPoints
+            )
+            binding.tasksHistoryButton.setOnClickListener {
+                onTasksHistoryClicked?.invoke(goal.goalId!!)
+            }
         }
     }
 }
